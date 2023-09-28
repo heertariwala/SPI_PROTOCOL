bc825ee5823de451166546cc1401d1a15e033273
heertariwala <heertariwala8@gmail.com>
2023-09-28 16:36:41 +0530

    add initial files

---
+++ SPI_RTL/rtl/spi/rtl/verilog/timescale.v
@@ -0,0 +1,13 @@
+/*#############################################################################################
+#                                                   					      #
+#                                                   					      #
+#############      Copyright (C) 2012-2020 VLSIGuru Training Institute        #################
+#                                                   					      #
+###### www.vlsiguru.com | contact@vlsiguru.com | +91-9986194191 | Horamavu, Bangalore  ########
+#                                                   					      #
+###### This Code is proprietary of VLSIGuru Training Institute. This code can't be reused #####
+###### or distributed without prior consent from VLSIGuru Training Institute.           #######
+#                                                   					      #
+#                                                   					      #
+##############################################################################################*/
+`timescale 1ns / 10ps
